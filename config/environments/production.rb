157eba113022540c57e793123483795652c4c960
EC2 Default User <ec2-user@ip-172-31-36-23.ap-northeast-1.compute.internal>
2020-02-09 13:14:10 +0000

    Modify production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default.delivery_method  :stmp
+  config.action_mailer.default.delivery_method = :stmp
   host = 'https://cryptic-earth-22340.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.stmp_settings = {
