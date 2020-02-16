a7747941741fb40f7f86e35509925316b01e319e
Your Name <ec2-user@ip-172-31-36-23.ap-northeast-1.compute.internal>
2020-02-16 09:03:13 +0000

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.initialize_on_precompile = false
+    
+    #認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
